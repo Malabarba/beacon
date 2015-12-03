74dc55ebaf2df109c949f840b481cbf678372cc8
Artur Malabarba <bruce.connor.am@gmail.com>
2015-12-03 16:52:21 +0000

    Fix weird beacon color when point coincides with an overlay

--- beacon.el
+++ beacon.el
@@ -255,7 +255,10 @@ Only returns `beacon-size' elements."
 
 (defun beacon--color-range ()
   "Return a list of background colors for the beacon."
-  (let* ((default-bg (or (background-color-at-point)
+  (let* ((default-bg (or (save-excursion
+                           (forward-line 1)
+                           (forward-char -1)
+                           (background-color-at-point))
                          (face-background 'default)))
          (bg (color-values (if (string-match "\\`unspecified-" default-bg)
                                (face-attribute 'beacon-fallback-background :background)
